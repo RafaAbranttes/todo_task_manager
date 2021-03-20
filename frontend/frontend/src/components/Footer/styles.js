8296d90090fba308a376017b5fa342f392428f49
Rafael Abrantes <rafael.vieira.abranttes@gmail.com>
2021-03-20 02:41:02 -0300

    Construção do footer

---
+++ frontend/frontend/src/components/Footer/styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 50px;
+  background: #090fcb;
+  display: flex;
+  border-top: 5px solid #ee8814;
+  position: fixed;
+  bottom: 0;
+  justify-content: center;
+
+  span {
+    color: white;
+    display: flex;
+    align-items: center;
+  }
+`;
