a3adfeccb1b66d68a7d0c296930b869c3fcf7ca9
Rafael Abrantes <rafael.vieira.abranttes@gmail.com>
2021-03-20 02:32:51 -0300

    Criação da parte do frontend

---
+++ frontend/frontend/README.md
@@ -0,0 +1,10 @@
+# FrontEnd
+
+* Parte frontend do gerenciador de tarefas utilizando ReactJs
+* Utilizando styled components
+
+* Para instalar o styled components
+
+```
+    npm install --save styled-components
+```
---
+++ frontend/frontend/src/views/Home/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import * as S from './styles'
+
+//components
+import Header from '../../components/Header'
+
+
+function Home() {
+  return (
+    <S.Container>
+      <Header></Header>
+    </S.Container>
+  )
+}
+
+export default Home;
